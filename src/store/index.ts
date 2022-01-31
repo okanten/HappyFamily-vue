ef0ac877d8125e63b01c7b341608f3f117b8f651
O.K <okgit@protonmail.com>
2022-01-31 17:35:44 +0100

    Adapted store-state to TypeScript

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,51 @@
-import { createStore } from 'vuex'
+import { InjectionKey } from 'vue'
+import { createStore, Store } from 'vuex'
+import { Mutations, mutations } from '@/store/mutations'
 
+export interface State {
+  defaultValue: string,
+  gameId: string,
+  gameIdPassword: string,
+  hasSubmittedWord: boolean,
+  hasLockedGame: boolean,
+  submittedWords: Array<string>,
+  instanceUrl: string,
+  apiUrl: string,
+}
+
+export const key: InjectionKey<Store<State>> = Symbol()
+
+export const store = createStore<State>({
+  state: {
+    defaultValue: 'loading...',
+    gameId: '',
+    gameIdPassword: '',
+    hasSubmittedWord: false,
+    hasLockedGame: false,
+    submittedWords: [],
+    instanceUrl: 'http://localhost:8080/',
+    apiUrl: 'http://localhost:8000'
+  },
+  mutations,
+})
+
+
+/*
+
+export type Store = Omit<VuexStore<any>, "commit"> & {
+ commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+   key: K,
+   payload: P,
+   options?: CommitOptions
+ ): ReturnType<Mutations[K]>;
+};
+
+*/
+
+/*
 const store = createStore({
   state() {
     return {
-      defaultValue: 'loading...',
-      gameId: null,
-      gameIdPassword: null,
-      hasSubmittedWord: false,
-      hasLockedGame: false,
-      submittedWords: [],
-      instanceUrl: 'http://localhost:8080/',
-      apiUrl: 'http://localhost:8000'
     }
   },
   mutations: {
@@ -38,3 +73,4 @@ const store = createStore({
 })
 
 export default store
+*/
---
+++ src/store/mutations.ts
@@ -0,0 +1,40 @@
+import { MutationTree } from "vuex"
+
+export enum MutationType {
+  SetGameId = "SET_GAME_ID",
+  SetGamePassword = "SET_GAME_PASSWORD",
+  SetSubmittedWord = "SET_SUBMITTED_WORD",
+  SetLockedGame = "SET_LOCKED_GAME",
+  SetSubmittedWords = "SET_SUBMITTED_WORDS",
+}
+
+export type Mutations = {
+  [MutationType.SetGameId](state: any, newValue: string): void;
+  [MutationType.SetGamePassword](state: any, newValue: string): void;
+  [MutationType.SetSubmittedWord](state: any, newValue: string): void;
+  [MutationType.SetLockedGame](state: any, newValue: Boolean): void;
+  [MutationType.SetSubmittedWords](state: any, newValue: Array<string>): void;
+}
+
+export const mutations: MutationTree<any> & Mutations = {
+  [MutationType.SetGameId](state, newValue) {
+    state.gameId = newValue
+  },
+
+  [MutationType.SetGamePassword](state, newValue) {
+    state.gameIdPassword = newValue
+  },
+  
+  [MutationType.SetSubmittedWord](state, newValue) {
+    state.hasSubmittedWord = newValue
+  },
+
+  
+  [MutationType.SetLockedGame](state, newValue) {
+    state.hasLockedGame = newValue
+  },
+  
+  [MutationType.SetSubmittedWords](state, newValue) {
+    state.submittedWords = newValue
+  }
+}
